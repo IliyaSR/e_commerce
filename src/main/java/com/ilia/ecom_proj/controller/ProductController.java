01a47786d591bd5c53e4fe3f8ce1a47a5ccd9ecb
unknown <insomniq4@abv.bg>
2025-08-07 18:12:56 +0600

    create get request by id and change returning type to ResponseEntity

--- src/main/java/com/ilia/ecom_proj/controller/ProductController.java
+++ src/main/java/com/ilia/ecom_proj/controller/ProductController.java
@@ -3,13 +3,14 @@ package com.ilia.ecom_proj.controller;
 import com.ilia.ecom_proj.model.Product;
 import com.ilia.ecom_proj.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class ProductController {
 
@@ -21,8 +22,20 @@ public class ProductController {
     }
 
     @GetMapping("/products")
-    public List<Product> getProducts() {
-        return service.getProducts();
+    public ResponseEntity<List<Product>> getProducts() {
+        return new ResponseEntity<>(service.getProducts(), HttpStatus.OK);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable int id) {
+
+        Product product = service.getProductById(id);
+
+        if (product != null) {
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
 }
