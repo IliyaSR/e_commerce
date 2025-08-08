437526e736a04e2121c114c68ec5762a75922756
unknown <insomniq4@abv.bg>
2025-08-08 17:35:16 +0600

    add fetching image

--- src/main/java/com/ilia/ecom_proj/controller/ProductController.java
+++ src/main/java/com/ilia/ecom_proj/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.ilia.ecom_proj.model.Product;
 import com.ilia.ecom_proj.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -48,5 +49,16 @@ public class ProductController {
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @GetMapping("/product/{productId}/image")
+    public ResponseEntity<byte[]> getProductImage(@PathVariable int productId){
+
+        Product product = service.getProductById(productId);
+        byte[] imageFile = product.getImageDate();
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.valueOf(product.getImageType()))
+                .body(imageFile);
     }
 }
